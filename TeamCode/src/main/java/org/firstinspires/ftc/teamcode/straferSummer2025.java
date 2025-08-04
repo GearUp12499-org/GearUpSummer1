31d444e0dbb42fda3f71c549b0c8884801c7ac02
elainew09 <elainewen09@gmail.com>
2025-08-03 18:57:21 -0700

    3 seconds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/straferSummer2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/straferSummer2025.java
@@ -62,120 +62,129 @@ public class straferSummer2025 extends LinearOpMode {
     @Override
         public void runOpMode() throws InterruptedException {
 
-            setHardware();
+        setHardware();
 
-            IMU imu=initializeIMU();
+        IMU imu = initializeIMU();
 
-            Servo servo = initializeServo();
+        Servo servo = initializeServo();
 
-            DistanceSensor  backRightDistance = hardwareMap.get(DistanceSensor.class, "backRightDistance");
-            DistanceSensor  backLeftDistance = hardwareMap.get(DistanceSensor.class, "backLeftDistance");
+        DistanceSensor backRightDistance = hardwareMap.get(DistanceSensor.class, "backRightDistance");
+        DistanceSensor backLeftDistance = hardwareMap.get(DistanceSensor.class, "backLeftDistance");
 
-            waitForStart();
+        waitForStart();
 
-            if (isStopRequested()) return;
+        if (isStopRequested()) return;
 
-            final double tgtDistance = 4;
-    //        final double allowedError = 1;
-            double motorPower = 0;
-            double i = 0;
-            double previous_error = 0;
-            double prev_time = 0;
-            final double k_p = 0.3; //change later
-            final double k_i = 0; //change later
-            final double k_d = 0; //change later
-            final double max_i = 1; //change later
+        final double tgtDistance = 4;
+        //        final double allowedError = 1;
+        double motorPower = 0;
+        double i = 0;
+        double previous_error = 0;
+        double prev_time = 0;
+        final double k_p = 0.05; //change later
+        final double k_i = 0; //change later
+        final double k_d = 0; //change later
+        final double max_i = 1; //change later
+        double maxPower = 1;
+        double delta_time = 0;
 
-            final double open_pos = 0.35;
-            final double close_pos = 0.2;
+        final double openPos = 0.35;
+        final double closePos = 0.2;
 
-            ElapsedTime runtime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
-            runtime.reset();
-            double period=100;//time in millisecond
-            while (opModeIsActive()) {
+        ElapsedTime runtime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+        runtime.reset();
+        double period = 100;//time in millisecond
+        while (opModeIsActive()) {
 
-                double current_time = runtime.time();
+            double current_time = runtime.time();
 
-                double leftDistance = backLeftDistance.getDistance(DistanceUnit.INCH);
-                double rightDistance = backRightDistance.getDistance(DistanceUnit.INCH);
+            double leftDistance = backLeftDistance.getDistance(DistanceUnit.INCH);
+            double rightDistance = backRightDistance.getDistance(DistanceUnit.INCH);
 
-                telemetry.addData("left range", leftDistance);
-                telemetry.addData("right range", rightDistance);
+            telemetry.addData("left range", leftDistance);
+            telemetry.addData("right range", rightDistance);
 
-                double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-                telemetry.addData("bot heading", botHeading);
+            telemetry.addData("bot heading", botHeading);
 
 
-                if (gamepad1.a) {
+            if (gamepad1.a) {
+                double startTime = runtime.time();
+                while (current_time - startTime < 3) {
                     telemetry.addData("a is pressed", "");
 
-                    double delta_time = current_time - prev_time;
+                    delta_time = current_time - prev_time;
                     double avgDistance = (leftDistance + rightDistance) / 2;
-                    double current_error = tgtDistance - avgDistance;
+                    double current_error = avgDistance - tgtDistance;
 
                     double p = k_p * current_error;
                     i += k_i * (current_error * (delta_time));
 
-                    if (i > max_i){
+                    if (i > max_i) {
                         i = max_i;
-                    }
-                    else if (i < -max_i){
+                    } else if (i < -max_i) {
                         i = -max_i;
                     }
 
                     double d = k_d * (current_error - previous_error) / delta_time;
 
                     motorPower = p + i + d;
-                    setMotorPower(motorPower,motorPower,motorPower,motorPower);
 
-                    previous_error=current_error;
+                    if (motorPower > maxPower) {
+                        motorPower = maxPower;
+                    } else if (motorPower < -(maxPower)) {
+                        motorPower = -maxPower;
+                    }
+                    setMotorPower(motorPower, motorPower, motorPower, motorPower);
+
+                    previous_error = current_error;
                     prev_time = current_time;
+                    current_time = runtime.time();
 
-//                    if (error > allowedError) {
-//                        setMotorPower(-motorPower,-motorPower,-motorPower,-motorPower);
-//                    } else if (error < -allowedError) {
-//                        setMotorPower(motorPower,motorPower,motorPower,motorPower);
-//                    }
-
-                } else if (gamepad1.options) {
-                    imu.resetYaw();
-                } else if (gamepad1.b) {
-                    servo.setPosition(open_pos);
-                } else if (gamepad1.x) {
-                    servo.setPosition(close_pos);
+                    //                    if (error > allowedError) {
+                    //                        setMotorPower(-motorPower,-motorPower,-motorPower,-motorPower);
+                    //                    } else if (error < -allowedError) {
+                    //                        setMotorPower(motorPower,motorPower,motorPower,motorPower);
+                    //                    }
                 }
-                else {
-                    double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-                    double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-                    double rx = gamepad1.right_stick_x;
+            } else if (gamepad1.options) {
+                imu.resetYaw();
+            } else if (gamepad1.b) {
+                servo.setPosition(openPos);
+            } else if (gamepad1.x) {
+                servo.setPosition(closePos);
+            } else {
+                double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+                double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+                double rx = gamepad1.right_stick_x;
 
 
-                    // Rotate the movement direction counter to the bot's rotation
-                    double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-                    double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+                // Rotate the movement direction counter to the bot's rotation
+                double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+                double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
-                    telemetry.addData("rot X", rotX);
-                    telemetry.addData("rot Y", rotY);
-                    telemetry.update();
+                telemetry.addData("rot X", rotX);
+                telemetry.addData("rot Y", rotY);
+                telemetry.update();
 
-                    // Denominator is the largest motor power (absolute value) or 1
-                    // This ensures all the powers maintain the same ratio,
-                    // but only if at least one is out of the range [-1, 1]
-                    double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-                    double frontLeftPower = (rotY + rotX + rx) / denominator;
-                    double backLeftPower = (rotY - rotX + rx) / denominator;
-                    double frontRightPower = (rotY - rotX - rx) / denominator;
-                    double backRightPower = (rotY + rotX - rx) / denominator;
-                    setMotorPower( frontLeftPower,backLeftPower,frontRightPower, backRightPower );
+                // Denominator is the largest motor power (absolute value) or 1
+                // This ensures all the powers maintain the same ratio,
+                // but only if at least one is out of the range [-1, 1]
+                double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+                double frontLeftPower = (rotY + rotX + rx) / denominator;
+                double backLeftPower = (rotY - rotX + rx) / denominator;
+                double frontRightPower = (rotY - rotX - rx) / denominator;
+                double backRightPower = (rotY + rotX - rx) / denominator;
+                setMotorPower(frontLeftPower, backLeftPower, frontRightPower, backRightPower);
 
-                }
+            }
 
-                double new_time = runtime.time();
-                while( new_time-current_time < period){
-                    Thread.yield();
-                    new_time = runtime.time();
-                }
+//                double new_time = runtime.time();
+//                while( new_time-current_time < period){
+//                    Thread.yield();
+//                    new_time = runtime.time();
+        }
 
 //
 //
@@ -200,7 +209,7 @@ public class straferSummer2025 extends LinearOpMode {
 //                        }
 //
 //                    }
-//                }
+//
 
 
 
@@ -208,5 +217,5 @@ public class straferSummer2025 extends LinearOpMode {
 
             }
         }
-    }
+
 
